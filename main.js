f9ce8ba18aa584f5b164720bbbfca4ffc5dd0229
An chang wan <ssdii66@gmail.com>
2022-02-16 23:12:07 +0900

    status-bar

---
+++ main.js
@@ -0,0 +1,24 @@
+const time = document.querySelector('.time');
+
+console.log(time);
+
+function getTime(){
+    const date = new Date();
+    const hour = date.getHours();
+    const minu = date.getMinutes();
+    const seconds =date.getSeconds();
+    if (seconds < 10){
+        time.innerHTML = `${hour}:${minu}:0${seconds}`;
+    }else{
+        time.innerHTML = `${hour}:${minu}:${seconds}`;
+    }
+
+    
+}
+
+getTime();
+
+setInterval(
+    getTime,
+    1000
+);
